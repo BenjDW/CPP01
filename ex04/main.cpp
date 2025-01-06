2ce28ae5606e961a28ad7a810e3e4d0fffeb25f2
vm_ubuntu42 <dewitskibenji@live.fr>
2025-01-06 13:43:51 -0800

    start and built the ideas

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,11 +6,39 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 14:05:49 by bde-wits          #+#    #+#             */
-/*   Updated: 2024/12/23 14:06:52 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:42:49 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+// Create a program that takes three parameters in the following order: a filename and
+// two strings, s1 and s2.
+// It will open the file <filename> and copies its content into a new file
+// <filename>.replace, replacing every occurrence of s1 with s2.
+// Of course, handle unexpected inputs and errors. You have to create and turn in your
+// own tests to ensure your program works as expected.
+
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+
+int	main(std::string filename, std::string s1, std::string s2)
 {
-	
+	std::ifstream	fd_in(filename);
+	std::ofstream	fd_out;
+	std::string		buffer;
+
+	if (!fd_in)
+	{
+		std::cout << "file not found" << std::endl;
+		return (1);
+	}
+	else
+	{
+		std::cout << "file found" << std::endl;
+	}
+	while(std::getline(fd_in, buffer))
+	{
+		
+	}
 }
